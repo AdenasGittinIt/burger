3bdb62ec981c4ca21cc2f97735fb91e5c6f17079
Adena McGibbon <justadena@gmail.com>
2019-12-14 21:31:45 -0700

    db added and seeded

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whisky River BBQ", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("5280 Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bad Daddy's", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Five Guys", true);
